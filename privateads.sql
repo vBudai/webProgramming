405bdee6e1c7ea9e00e8e6844052b71436ba923c
Будай Владислав <vbudai297@gmail.com>
2022-03-15 00:06:33 +0500

    upgraded database

--- privateads.sql
+++ privateads.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Мар 12 2022 г., 19:46
+-- Время создания: Мар 14 2022 г., 21:43
 -- Версия сервера: 5.7.33-log
 -- Версия PHP: 7.1.33
 
@@ -31,25 +31,25 @@ CREATE TABLE `ad` (
   `id` int(11) UNSIGNED NOT NULL COMMENT 'ID объявления',
   `id_user` int(10) UNSIGNED NOT NULL COMMENT 'ID пользователя',
   `title` varchar(255) NOT NULL COMMENT 'Название',
-  `information` text COMMENT 'Текст',
+  `information` text COMMENT 'Описание объявления',
   `img` text COMMENT 'Изображение',
   `creationTime` datetime NOT NULL COMMENT 'Время создания объявления',
-  `id_category` int(10) UNSIGNED NOT NULL COMMENT 'Категория'
+  `price` int(10) UNSIGNED NOT NULL COMMENT 'Цена товара'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Объявление';
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `category`
+-- Структура таблицы `message`
 --
 
-CREATE TABLE `category` (
-  `id` int(11) UNSIGNED NOT NULL COMMENT 'ID категории',
-  `mainCategory` set('Продажа','Работа','Услуги') NOT NULL COMMENT 'Главная категория',
-  `sale` set('Транспорт','Недвижимость','Личные вещи','Электроника','Животные','Хобби и отдых') DEFAULT NULL COMMENT 'Подкатегория "Продажа"',
-  `services` set('Транспорт/Перевозки','Праздники/Мероприятия','Ремонт/Строительство','Красота/Здоровье','Бытовые услуги','IT','Обучение','Установка техники','Ремонт техники') DEFAULT NULL COMMENT 'Подкатегория "Услуги"',
-  `job` set('Вакансии','Резюме') DEFAULT NULL COMMENT 'Подкатегория "Работа"'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Категория объявления';
+CREATE TABLE `message` (
+  `id` int(10) UNSIGNED NOT NULL COMMENT 'ID сообщения',
+  `id_ad` int(10) UNSIGNED NOT NULL COMMENT 'ID объявления',
+  `id_user` int(10) UNSIGNED NOT NULL COMMENT 'ID пользователя',
+  `messageText` text NOT NULL COMMENT 'Текст сообщения',
+  `sendTime` datetime NOT NULL COMMENT 'Время отправки сообщения'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Сообщения под объявлением';
 
 -- --------------------------------------------------------
 
@@ -68,6 +68,14 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `firstName`, `lastName`, `md5Password`, `isAdmin`, `phoneNumber`, `email`) VALUES
+(1, 'Владислав', 'Будай', '201cb962ac59075b964b07152d234b71', 1, '79123456789', 'qwert@gmail.com'),
+(2, 'Анатолий', 'Шаратов', '202cb962ac59075b964b07152d234b70', 0, '79987654321', 'trewq@mail.ru');
+
+--
 -- Индексы сохранённых таблиц
 --
 
@@ -76,14 +84,15 @@ CREATE TABLE `user` (
 --
 ALTER TABLE `ad`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_category` (`id_category`);
+  ADD KEY `id_user` (`id_user`);
 
 --
--- Индексы таблицы `category`
+-- Индексы таблицы `message`
 --
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_ad` (`id_ad`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Индексы таблицы `user`
@@ -102,16 +111,16 @@ ALTER TABLE `ad`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID объявления';
 
 --
--- AUTO_INCREMENT для таблицы `category`
+-- AUTO_INCREMENT для таблицы `message`
 --
-ALTER TABLE `category`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID категории';
+ALTER TABLE `message`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID сообщения';
 
 --
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID пользователя';
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID пользователя', AUTO_INCREMENT=5;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -121,8 +130,14 @@ ALTER TABLE `user`
 -- Ограничения внешнего ключа таблицы `ad`
 --
 ALTER TABLE `ad`
-  ADD CONSTRAINT `ad_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `ad_ibfk_2` FOREIGN KEY (`id_category`) REFERENCES `category` (`id`);
+  ADD CONSTRAINT `ad_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`id_ad`) REFERENCES `ad` (`id`),
+  ADD CONSTRAINT `message_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
